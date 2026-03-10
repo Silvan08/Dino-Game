eb8d77a51b1ebb2e3ac1118f6f40c01e6830cf97
Silvan08 <silvan_bachmann@sluz.ch>
2026-03-10 21:14:44 +0100

    Dino 2
    Dino 2

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const hog = document.getElementById("hog")
 const rock = document.getElementById("rock")
 const score = document.getElementById("score")
 
-
-
 function jump() {
   hog.classList.add("jump-animation")
   setTimeout(() => hog.classList.remove("jump-animation"), 750)
@@ -15,16 +13,24 @@ document.addEventListener("keypress", (event) => {
   }
 })
 
+function handleCollision() {
+  if (hog.classList.contains("hit")) return
+
+  hog.classList.add("hit")
+  document.getElementById("game").classList.add("hit")
+
+  hog.addEventListener("animationend", () => {
+    hog.classList.remove("hit")
+    document.getElementById("game").classList.remove("hit")
+  }, { once: true })
+
+  score.innerText = 0
+}
 
 setInterval(() => {
-  const hogTop = parseInt(
-    window.getComputedStyle(hog).getPropertyValue("top"),
-  )
-  const rockLeft = parseInt(
-    window.getComputedStyle(rock).getPropertyValue("left"),
-  )
-  score.innerText = parseInt(score.innerText) + 2;
-
+  const hogTop = parseInt(window.getComputedStyle(hog).getPropertyValue("top"))
+  const rockLeft = parseInt(window.getComputedStyle(rock).getPropertyValue("left"))
+  score.innerText = parseInt(score.innerText) + 2
 
   if (rockLeft < 0) {
     rock.style.display = "none"
@@ -33,6 +39,6 @@ setInterval(() => {
   }
 
   if (rockLeft < 50 && rockLeft > 0 && hogTop > 150) {
-  score.innerText = 0
+    handleCollision()
   }
 }, 50)
